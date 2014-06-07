c5080b0b9b6ecf5712f9233e4d828162412aec97
Scott Bronson <brons_git@rinspin.com>
2014-06-06 19:07:33 -0700

    output of bundle init

---
+++ lib/geocode/static.rb
@@ -0,0 +1,7 @@
+require "geocode/static/version"
+
+module Geocode
+  module Static
+    # Your code goes here...
+  end
+end
---
+++ lib/geocode/static/version.rb
@@ -0,0 +1,5 @@
+module Geocode
+  module Static
+    VERSION = "0.0.1"
+  end
+end
