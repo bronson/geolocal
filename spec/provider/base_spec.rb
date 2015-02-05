74b38d2a80d6b554f40ed8dd232b1fefb5a88fa6
Scott Bronson <brons_git@rinspin.com>
2015-02-05 01:22:11 -0800

    test generated exceptions if v4 or v6 isn't enabled

--- spec/provider/base_spec.rb
+++ spec/provider/base_spec.rb
@@ -19,32 +19,57 @@ describe Geolocal::Provider::Base do
   end
 
   it 'can generate both ipv4 and ipv6' do
+    Geolocal.configure do |config|
+      config.module = 'Geolocal_v4v6'
+    end
+
     io = StringIO.new
     provider.output io, example_results
     expect(io.string).to include '0..42540528726795050063891204319802818559'
     expect(io.string).to include '34605568..34606591'
+
+    eval io.string
+    expect(Geolocal_v4v6.in_us? '2.16.13.255').to eq true
+    expect(Geolocal_v4v6.in_us? IPAddr.new('2.16.13.255')).to eq true
+    expect(Geolocal_v4v6.in_us? 34606591, Socket::AF_INET).to eq true
+    expect(Geolocal_v4v6.in_us? 34606592, Socket::AF_INET).to eq nil
+
+    expect(Geolocal_v4v6.in_us? '2001:400::').to eq true
+    expect(Geolocal_v4v6.in_us? IPAddr.new('2001:400::')).to eq true
+    expect(Geolocal_v4v6.in_us? 42540570559264857595461453711008595967, Socket::AF_INET6).to eq true
+    expect(Geolocal_v4v6.in_us? 42540570559264857595461453711008595968, Socket::AF_INET6).to eq nil
   end
 
   it 'can turn off ipv4' do
     Geolocal.configure do |config|
       config.ipv4 = false
+      config.module = 'Geolocal_v6'
     end
 
     io = StringIO.new
     provider.output io, example_results.tap { |h| h.delete('USv4') }
     expect(io.string).to include '0..42540528726795050063891204319802818559'
     expect(io.string).not_to include '34605568..34606591'
+
+    eval io.string
+    expect{ Geolocal_v6.in_us? '2.16.13.255' }.to raise_error(/ipv4 was not compiled in/)
+    expect( Geolocal_v6.in_us? '2001:400::'  ).to eq true
   end
 
   it 'can turn off ipv6' do
     Geolocal.configure do |config|
       config.ipv6 = false
+      config.module = 'Geolocal_v4'
     end
 
     io = StringIO.new
     provider.output io, example_results.tap { |h| h.delete('USv6') }
     expect(io.string).to include '34605568..34606591'
     expect(io.string).not_to include '0..42540528726795050063891204319802818559'
+
+    eval io.string
+    expect{ Geolocal_v4.in_us? '2001:400::'  }.to raise_error(/ipv6 was not compiled in/)
+    expect( Geolocal_v4.in_us? '2.16.13.255' ).to eq true
   end
 
   it 'can turn off both ipv4 and ipv6' do
@@ -57,27 +82,5 @@ describe Geolocal::Provider::Base do
     provider.output io, example_results.tap { |h| h.delete('USv4'); h.delete('USv6') }
     expect(io.string).not_to include '34605568..34606591'
     expect(io.string).not_to include '0..42540528726795050063891204319802818559'
-  end
-
-  it 'can execute the resulting file' do
-    modname = "Geolocal_#{rand(1000000)}"
-    Geolocal.configure do |config|
-      config.module = modname
-    end
-
-    io = StringIO.new
-    provider.output io, example_results
-    eval io.string
-    geolocal = Module.const_get(modname)
-
-    expect(geolocal.in_us? '2.16.13.255').to eq true
-    expect(geolocal.in_us? IPAddr.new('2.16.13.255')).to eq true
-    expect(geolocal.in_us? 34606591, Socket::AF_INET).to eq true
-    expect(geolocal.in_us? 34606592, Socket::AF_INET).to eq nil
-
-    expect(geolocal.in_us? '2001:400::').to eq true
-    expect(geolocal.in_us? IPAddr.new('2001:400::')).to eq true
-    expect(geolocal.in_us? 42540570559264857595461453711008595967, Socket::AF_INET6).to eq true
-    expect(geolocal.in_us? 42540570559264857595461453711008595968, Socket::AF_INET6).to eq nil
   end
 end
