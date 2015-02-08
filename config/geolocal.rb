55f5cc9a2520f0eef05005d7dedb07861d057651
Scott Bronson <brons_git@rinspin.com>
2015-02-07 21:06:04 -0800

    github SUCKS at symlinks.  guess I need to reverse these

--- config/geolocal.rb
+++ config/geolocal.rb
@@ -1,1 +1,17 @@
-../lib/generators/geolocal/templates/geolocal.rb
+require 'geolocal'
+
+Geolocal.configure do |config|
+  # This example creates a Geolocal.in_us?(addr) command:
+  config.countries = { us: 'US' }
+
+  # This is where the output goes.  You should commit it to your project.
+  # config.module = 'Geolocal'
+  # config.file = 'lib/geolocal.rb'
+
+  # If your host is IPv4 only, you can save space by omitting IPv6 data and vice versa.
+  # config.ipv6 = true
+  # config.ipv4 = true
+
+  # Download and process the geocoding data in this directory.  Don't commit it.
+  # config.tmpdir = './tmp/geolocal'
+end
