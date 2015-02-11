ca961c97e91d8ca1678898bd672844741839cfe2
Scott Bronson <brons_git@rinspin.com>
2015-02-10 23:27:21 -0800

    nope, geolocal.rb file must not exist at all
    Still trying to fix the geolocal.rb ambiguity.

--- lib/geolocal.rb
+++
@@ -1,5 +0,0 @@
-# If you're wondering why nothing's here, you probably wanted to require 'geolocal/configuration'
-# Once you define a configuration and generate the geolocation code, this module will have contents.
-module Geolocal
-  require 'geolocal/railtie' if defined? Rails
-end
